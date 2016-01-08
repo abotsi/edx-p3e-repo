f0a3f971f47912aff96344bd07debe5cd51e73ee
abotsi <abotsi@users.noreply.github.com>
2016-01-08 20:14:45 +0100

    Mise à jour de la version du package python (egg)

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def package_data(pkg, roots):
 
 setup(
     name='p3exblock-xblock',
-    version='0.1',
+    version='0.2',
     description='p3exblock XBlock',   # TODO: write a better description.
     packages=[
         'p3exblock',
